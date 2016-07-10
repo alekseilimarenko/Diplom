0df2d045e7a272d2abbb3e2104b550573a2f5f5c
morfei74 <vega74@ua.fm>
2016-07-10 16:42:56 +0300

    Create PropertyReader.java

---
+++ Server/MainFolder/PropertyReader.java
@@ -0,0 +1,31 @@
+package server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyReader
+{
+    private static Properties prop;
+    private static String[] conf;
+    
+    public static void InitComponents() throws IOException
+    {
+        prop = new Properties();
+        
+        prop.load(new FileInputStream(new File("Connect.ini")));
+        
+        conf = prop.getProperty("Connect").split(";");
+    }
+    
+    public static String GetAdress()
+    {
+        return conf[0];
+    }
+    
+    public static int GetPort()
+    {
+        return Integer.parseInt(conf[1]);
+    }
+}
